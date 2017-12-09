c48ab72b0745f3788b5b94661a87a14648e863a0
Anmol Sharma <33972053+AnmolSharma1992@users.noreply.github.com>
2017-12-08 23:54:44 -0500

    Add files via upload

---
+++ Practice Code.py
@@ -0,0 +1,15 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> # basic output
+>>> 1+2
+3
+>>> # basic computation
+>>> 1.54/4.78
+0.32217573221757323
+>>> # floats
+>>> 2**6
+64
+>>> # exponentiation
+>>> 
